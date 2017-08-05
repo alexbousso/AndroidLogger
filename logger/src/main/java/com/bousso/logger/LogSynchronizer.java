3242d2f226367b0ed18cb245c355e7608aa3c9d8
Alex Bousso <alexbousso@gmail.com>
2017-08-05 14:07:34 +0300

    Partially implemented LogSynchronizer

--- logger/src/main/java/com/bousso/logger/LogSynchronizer.java
+++ logger/src/main/java/com/bousso/logger/LogSynchronizer.java
@@ -1,14 +1,54 @@
 package com.bousso.logger;
 
-/**
- * Created by eytan on 05/08/17.
- */
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 class LogSynchronizer {
-    public static void trace(String message, String component) {}
-    public static void debug(String message, String component) {}
-    public static void verbose(String message, String component) {}
-    public static void warning(String message, String component) {}
-    public static void error(String message, String component) {}
-    public static void critical(String message, String component) {}
+    private static boolean isInitialized = false;
+    private Context context;
+    private LinkedBlockingQueue<LogMessage> inputQueue = new LinkedBlockingQueue<>();
+    private Thread writer;
+
+    @SuppressLint("StaticFieldLeak")
+    private static LogSynchronizer instance = null;
+
+    protected LogSynchronizer() {}
+
+    public static LogSynchronizer getInstance() {
+        if (instance == null) {
+            instance = new LogSynchronizer();
+        }
+        return instance;
+    }
+
+    public void initialize(Context context) {
+        if (isInitialized) {
+            return;
+        }
+
+        this.context = context;
+        writer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //noinspection InfiniteLoopStatement
+                while (true) {
+                    try {
+                        LogMessage message = inputQueue.poll(1, TimeUnit.DAYS);
+                    } catch (InterruptedException e) { }
+                }
+            }
+        });
+        writer.start();
+        isInitialized = true;
+    }
+
+    public void write(LogMessage message) {
+        if (!isInitialized) {
+            throw new RuntimeException("LogSynchronizer must be initialized. Use initialize()");
+        }
+        inputQueue.add(message);
+    }
 }
