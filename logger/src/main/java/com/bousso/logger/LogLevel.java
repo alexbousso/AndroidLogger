27a0e8b0e7706cccdc97a982bde2c34490ca816b
Eytan Singher <eytan.singher@gmail.com>
2017-08-05 12:19:25 +0300

    Added empty interface for synchronizer
    Signed-off-by: Eytan Singher <eytan.singher@gmail.com>

---
+++ logger/src/main/java/com/bousso/logger/LogLevel.java
@@ -0,0 +1,16 @@
+package com.bousso.logger;
+
+import android.util.Log;
+
+/**
+ * Created by eytan on 05/08/17.
+ */
+
+public enum LogLevel {
+    Trace,
+    Debug,
+    Verbose,
+    Warning,
+    Error,
+    Critical;
+}
---
+++ logger/src/main/java/com/bousso/logger/LogSynchronizer.java
@@ -0,0 +1,14 @@
+package com.bousso.logger;
+
+/**
+ * Created by eytan on 05/08/17.
+ */
+
+class LogSynchronizer {
+    public static void trace(String message, String component) {}
+    public static void debug(String message, String component) {}
+    public static void verbose(String message, String component) {}
+    public static void warning(String message, String component) {}
+    public static void error(String message, String component) {}
+    public static void critical(String message, String component) {}
+}
